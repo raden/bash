59c845d940ac1fd60a8ad80be068b96507750c2f
Muhammad Najmi Ahmad Zabidi <raden@users.noreply.github.com>
2018-10-29 15:24:22 +0800

    Removed IP (sort of IP anonimizer)

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,5 @@
 #for i in `seq 1`;do echo Test $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -e "s/([^(]*)//"; done
-
-for i in `seq 1 2`;do echo Test $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -r 's/\([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\)//g'; done
+printf "Saya akan buat ujian speedtest sebanyak $1 kali\n"
+#for i in `seq 1 $1`;do echo Ujian $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -r 's/\([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\)//g'; done
+for i in `seq 1 $1`;do echo Ujian $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -e "s/([^)]*)//"; done
 

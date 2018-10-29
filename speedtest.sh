fb0c760850870aecceb895ee52b2705dc005cd70
Muhammad Najmi Ahmad Zabidi <raden@users.noreply.github.com>
2018-10-29 22:52:46 +0800

    Add files via upload
    Speedtest with Gnuplot capability

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,29 @@
-#for i in `seq 1`;do echo Test $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -e "s/([^(]*)//"; done
-printf "Saya akan buat ujian speedtest sebanyak $1 kali\n"
-#for i in `seq 1 $1`;do echo Ujian $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -r 's/\([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\)//g'; done
-for i in `seq 1 $1`;do echo Ujian $i; speedtest|grep "Testing\|Download"|grep -v speed|sed -e "s/([^)]*)//"; done
+#!/usr/bin/bash
+rm /tmp/unifi-speedtest*.txt
 
+printf "Saya akan buat ujian speedtest sebanyak $1 kali\n"
+
+for i in `seq 1 $1`;do echo Ujian $i|tee -a /tmp/unifi-speedtest3.txt; speedtest|grep "Testing\|Download"|grep -v speed|sed -e "s/([^)]*)//" |tee -a /tmp/unifi-speedtest.txt; done
+
+cat /tmp/unifi-speedtest.txt|awk {'print $2'}|grep -v from > /tmp/unifi-speedtest2.txt
+
+step=$(wc -l /tmp/unifi-speedtest2.txt|awk {'print $1'})
+
+sed -i 's/Ujian//g' /tmp/unifi-speedtest3.txt
+
+#for i in `seq 1 $step`;do echo $i|tee -a /tmp/unifi-speedtest3.txt;done
+
+paste /tmp/unifi-speedtest3.txt /tmp/unifi-speedtest2.txt > /tmp/unifi-speedtest4.txt
+
+cat /tmp/unifi-speedtest4.txt
+
+rm -f /tmp/unifi.plot
+
+echo "set xlabel \"Ujian\" " >> /tmp/unifi.plot
+echo "set ylabel \"Kelajuan\" " >> /tmp/unifi.plot
+echo "set yrange [0:10]" >> /tmp/unifi.plot
+echo "set xrange [0:$step]" >> /tmp/unifi.plot
+
+echo "plot \"/tmp/unifi-speedtest4.txt\" with linespoint " >> /tmp/unifi.plot
+
+gnuplot -p /tmp/unifi.plot
